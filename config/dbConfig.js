be2e9c91b690872e85ce668776eb84255975e529
zacharyenright <zachary.enright1@gmail.com>
2024-10-30 13:06:12 -0400

    changed to relative paths

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // Absolute path should not be joined with __dirname
-const dbPath = '/Users/zacharyenright/cybr_group_project/cybr3410_group_project-1/databases/database.db';
+const dbPath = 'databases/database.db';
 
 // Initialize the database connection
 const db = new sqlite3.Database(dbPath, (err) => {
